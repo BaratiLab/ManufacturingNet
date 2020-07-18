6b98e1376179ac7e445529533e1c78c7f9802af0
Ruchit <doshiruchit@gmail.com>
2020-07-18 17:13:39 -0400

    Pypi_Package

---
+++ pypi_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+package_name =''
+
+setup(name=package_name,\
+    version='1.1',\
+        description='Manufacturing related work',
+        packages=[package_name],
+        author='Mail_lab_cmu',
+        author_email='ruchitsd@andrew.cmu.edu',
+        zip_safe=False)
