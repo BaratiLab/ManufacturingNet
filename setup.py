ddb9b4fca1a6f36ec66d614d1c840e35d5577396
Ruchit <doshiruchit@gmail.com>
2020-07-18 23:43:23 -0400

    ManufacturingNet_package

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup,find_packages
 
 package_name ='ManufacturingNet'
 
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
 classifiers=[
     'Programming Language :: Python :: 3.7',
-    'License :: OSI Approved :: MIT license',
     'Intended Audience :: Education']
 
 setup(name=package_name,\
     version='0.0.1',\
         description='AI and Machine Learning for manufacturing related datasets',
-        long_description=open('README.md').read(),
-        url-'manufacturingnet.io',
+        long_description=LONG_DESCRIPTION,
         author='Ruchti Doshi',
         author_email='ruchitsd@andrew.cmu.edu',
         license='MIT',
         classifiers=classifiers,
         keywords=' ',
-        packages=find_packages(),
+        packages=["ManufacturingNet"],
         zip_safe=False)
