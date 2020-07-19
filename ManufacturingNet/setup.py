b88340b903cbbb5cd96b715f322e10f740c9d583
Ruchit <doshiruchit@gmail.com>
2020-07-19 00:05:31 -0400

    Ignoring_files

--- ManufacturingNet/setup.py
+++ ManufacturingNet/setup.py
@@ -6,7 +6,12 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 classifiers=[
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only',
     'Intended Audience :: Education']
 
 setup(name=package_name,\
@@ -18,5 +23,6 @@ setup(name=package_name,\
         license='MIT',
         classifiers=classifiers,
         keywords=' ',
-        packages=["ManufacturingNet"],
+        python_requires='>=3.5, <4',
+        packages=find_packages(),
         zip_safe=False)
