3265b4ad90a15cb33c5e7e603c7daf7e0d8c2a1f
Lalit Ghule <58838043+lalitjg@users.noreply.github.com>
2020-08-13 16:54:31 -0400

    Update __init__.py
    Adding CNNLSTM code

--- ManufacturingNet/deep_learning_methods/__init__.py
+++ ManufacturingNet/deep_learning_methods/__init__.py
@@ -1,4 +1,5 @@
 from .DNN import DNNModel
 from .CNN import CNN2D
 from .CNN3D import CNN3D
-__all__ = ['DNNModel', 'CNN2D','CNN3D']
+from .CNN_LSTM_Default import CNNLSTMModel
+__all__ = ['DNNModel', 'CNN2D', 'CNN3D', 'CNLSTMModel']
