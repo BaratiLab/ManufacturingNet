49ed66a41ca218fa95da8dbeffc7679f2db46825
Ruchit <doshiruchit@gmail.com>
2020-08-17 10:03:56 -0400

    Googlenet

--- ManufacturingNet/pretrained_models/__init__.py
+++ ManufacturingNet/pretrained_models/__init__.py
@@ -2,5 +2,7 @@ from .resnet import ResNet
 from .vgg import VGG
 from .densenet import DenseNet
 from .alexnet import AlexNet
+from .googlenet import GoogleNet
+from .mobilenet import MobileNet
 
-__all__=['ResNet','VGG','DenseNet','AlexNet']
+__all__=['ResNet','VGG','DenseNet','AlexNet','MobileNet'.'GoogleNet']
