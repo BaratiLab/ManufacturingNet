4c5aea6f2a0028573d17ead53bf8bb580e853275
Ruchit <doshiruchit@gmail.com>
2020-08-18 18:55:35 -0400

    errors

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers=[
     'Intended Audience :: Education']
 
 setup(name=package_name,\
-    version='0.0.9',\
+    version='0.1.0',\
         description='AI and Machine Learning for manufacturing related datasets',
         long_description=LONG_DESCRIPTION,
         author='Ruchti Doshi',
