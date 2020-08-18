aa488aa9c46dd099d9c35add9e81a12eee453577
Ruchit <doshiruchit@gmail.com>
2020-08-18 12:11:02 -0400

    Verion_9

--- ManufacturingNet/deep_learning_methods/__init__.py
+++ ManufacturingNet/deep_learning_methods/__init__.py
@@ -2,4 +2,4 @@ from .DNN import DNNModel
 from .CNN import CNN2D
 from .CNN3D import CNN3D
 from .CNN_LSTM_Default import CNNLSTMModel
-__all__ = ['DNNModel', 'CNN2D', 'CNN3D', 'CNLSTMModel']
+__all__ = ['DNNModel', 'CNN2D', 'CNN3D', 'CNNLSTMModel']
