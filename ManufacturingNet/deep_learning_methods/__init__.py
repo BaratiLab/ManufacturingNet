07b25397d08f783fb40f5b038c31c6585e58cb72
Lalit Ghule <58838043+lalitjg@users.noreply.github.com>
2020-08-23 19:33:12 -0400

    Update __init__.py
    LSTM code

--- ManufacturingNet/deep_learning_methods/__init__.py
+++ ManufacturingNet/deep_learning_methods/__init__.py
@@ -3,4 +3,5 @@ from .CNN import CNN2D
 from .CNN3D import CNN3D
 from .CNN_LSTM_Default import CNNLSTMModel
 from .CNN2DImage import CNN2DImageModel
-__all__ = ['DNNModel', 'CNN2D', 'CNN3D', 'CNNLSTMModel', 'CNN2DImageModel']
+from .LSTM_Default import LSTMModel
+__all__ = ['DNNModel', 'CNN2D', 'CNN3D', 'CNNLSTMModel', 'CNN2DImageModel', 'LSTMModel']
