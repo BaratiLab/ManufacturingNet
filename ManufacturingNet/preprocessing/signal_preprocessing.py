13f90846092d969e63ae544afdfd8f55fa81c6b1
Aman Khalid <aakhalid@umich.edu>
2020-09-17 20:36:11 -0400

    Cleaned signal_preprocessing.py

--- ManufacturingNet/preprocessing/signal_preprocessing.py
+++ ManufacturingNet/preprocessing/signal_preprocessing.py
@@ -1,126 +1,97 @@
+"""signal_preprocessing is a module for performing preprocessing steps
+like normalizing, data cleaning, and transforming. The normalizations
+currently supported are mean, min-max, and quantile.
+
+Read the documentation at https://manufacturingnet.readthedocs.io.
+"""
+
 import numpy as np
 
-class MeanNormalizer():
-    """
-    This class performs normalizaation of the data along the given axis. It calculates the mean and standard deviation along the axis.
-    It subtracts the mean and divides by the standard deviation.
+class MeanNormalizer:
+    """This class normalizes the data along the given axis. It first
+    calculates the mean and standard deviation. It then subtracts the
+    mean from each value and divides by the standard deviation.
 
-    This transformation sets the mean of data to 0 and the standard deviation to 1
-    
-    
+    This transformation sets the mean of the data to 0 and the standard
+    deviation to 1.
     """
-    def __init__(self, a, axis = 0):
-        
-        self.data = a
-        self.axis = axis
-        self.data_mean = np.mean(self.data, self.axis)
-        self.data_std = np.std(self.data, self.axis)
-        self.normalized_data = (self.data - self.data_mean)/(self.data_std) 
-        self.scaled_data = None 
-        
+
+    def __init__(self, data, axis=0):
+        """Initializes a MeanNormalizer object."""
+        self.data_mean = np.mean(data, axis)
+        self.data_std = np.std(data, axis)
+        self.normalized_data = (data - self.data_mean) / (self.data_std)
+        self.scaled_data = None
 
     def get_normalized_data(self):
-
+        """Accessor method for the normalized data.
+        Returns an array of normalized data along the given axis.
         """
-        Accessor method for getting normalized data
-
-        Returns an array of normalized data along the given axis
-        
-        """
-        
-
         return self.normalized_data
-    
+
     def get_scaled_data(self, test_data):
-
+        """Accessor method for getting the scaled data.
+        Returns an array of the normalized data along the given axis.
         """
-        Accessor method for getting the scaled data
-
-        Returns an array of normalized data along the given axis or None.
-
-        """
-        self.scaled_data = (test_data*(self.data_std)) + self.data_mean
-
+        self.scaled_data = (test_data * self.data_std) + self.data_mean
         return self.scaled_data
 
-
-
-class MinMaxNormalizer():
+class MinMaxNormalizer:
+    """This class normalizes the data along the given axis. It first
+    calculates the min and max values along the axis. Then, it subtracts
+    the min from each value and divides by the range.
     """
-    This class performs normalization of the data along the given axis. It calculates the min and max values along the axis.
-    It subtracts the min and divides by the range (range = max - min).
-    
-    """
-    def __init__(self, a, axis = 0):
-        
-        self.data = a
-        self.axis = axis
-        self.data_min = np.min(self.data, self.axis)
-        self.data_max = np.max(self.data, self.axis)
-        self.scaled_data = None 
-        self.normalized_data = (self.data - self.data_min)/(self.data_max -self.data_min) 
+
+    def __init__(self, data, axis=0):
+        """Initializes a MinMaxNormalizer object."""
+        self.data_min = np.min(data, axis)
+        self.data_max = np.max(data, axis)
+        self.normalized_data = \
+            (data - self.data_min) / (self.data_max - self.data_min)
+        self.scaled_data = None
 
     def get_normalized_data(self):
-
-        """
-        Accessor method for getting normalized data
-
-        Returns an array of normalized data along the given axis
+        """Accessor method for getting normalized data.
+        Returns an array of normalized data along the given axis.
         """
         return self.normalized_data
-    
+
     def get_scaled_data(self, test_data):
-        
+        """Accessor method for getting the scaled data.
+        Returns an array of normalized data along the given axis.
         """
-        Accessor method for getting the scaled data
-
-        Returns an array of normalized data along the given axis or None.
-
-        """
-        self.scaled_data = (test_data*(self.data_max - self.data_min)) + self.data_min
-
+        self.scaled_data = \
+            (test_data * (self.data_max - self.data_min)) + self.data_min
         return self.scaled_data
 
+class QuantileNormalizer:
+    """Scaling using median and quantiles consists of subtracting the
+    median from all observations and dividing by the interquartile
+    difference. It Scales features using statistics that are robust to
+    outliers.
 
-class QuantileNormalizer():
+    Default interquartile range is 0.25-0.75 (q1-q3).
     """
-    Scaling using median and quantiles consists of subtracting the median to all the observations and then dividing by the interquartile difference. 
-    It Scales features using statistics that are robust to outliers.
-    Default inter-quartile range is 0.25 - 0.75 (q1 - q2).
-    
-    
-    """
-    def __init__(self, a, axis = 0, q1 = 0.25, q2 = 0.75):
-        
+
+    def __init__(self, data, axis=0, q1=0.25, q3=0.75):
+        """Initializes a QuantileNormalizer object."""
         assert q1 > 0 and q1 < 1
-        assert q2 > q1 and q2 < 1
-        
-        self.data = a
-        self.axis = axis
-        self.IQR = np.percentile(self.data, q2, self.axis) - np.percentile(self.data, q1, self.axis)
-        self.data_median = np.median(self.data, self.axis)
-        self.scaled_data = None 
-        self.normalized_data = (self.data - self.data_median)/(self.IQR) 
+        assert q3 > q1 and q3 < 1
+
+        self.IQR = np.percentile(data, q3, axis) - np.percentile(data, q1, axis)
+        self.data_median = np.median(data, axis)
+        self.normalized_data = (data - self.data_median) / (self.IQR)
+        self.scaled_data = None
 
     def get_normalized_data(self):
-
-        """
-        Accessor method for getting normalized data
-
-        Returns an array of normalized data along the given axis
-        
-        
+        """Accessor method for getting normalized data.
+        Returns an array of normalized data along the given axis.
         """
         return self.normalized_data
-    
+
     def get_scaled_data(self, test_data):
-        
+        """Accessor method for getting the scaled data.
+        Returns an array of normalized data along the given axis.
         """
-        Accessor method for getting the scaled data
-
-        Returns an array of normalized data along the given axis or None.
-
-        """
-        self.scaled_data = (test_data*(self.IQR)) + self.data_median
-
+        self.scaled_data = (test_data * self.IQR) + self.data_median
         return self.scaled_data
