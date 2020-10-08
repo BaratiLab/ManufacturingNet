eda2acb4726559fbde805a216860b1e75bb02e82
Ruchit Doshi <doshiruchit12@gmail.com>
2020-10-08 09:48:22 -0700

    Datasets

---
+++ ManufacturingNet/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .datasets import PaderbornBearingData,CWRUBearingData
+from .datasets import CastingData,ChatterData  
+from .datasets import MercedesData, ThreeDPrintingData, MotorTempData, LithographyData
+from .datasets import GearboxData
+from .datasets import SpatterData
+
+__all__=['PaderbornBearingData','CWRUBearingData','CastingData','ChatterData','MercedesData', 'ThreeDPrintingData', 'MotorTempData', 'LithographyData','GearboxData','SpatterData']
