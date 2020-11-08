7bde7dcb653cab2f5ccfe020127fa7b0494c2b4f
Ruchit <doshiruchit@gmail.com>
2020-11-08 11:29:41 -0500

    1.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers=[
     'Intended Audience :: Education']
 
 setup(name=package_name,\
-    version='0.1.6',\
+    version='0.1.7',\
         description='AI and Machine Learning for manufacturing related datasets',
         long_description=LONG_DESCRIPTION,
         author='Ruchti Doshi',
