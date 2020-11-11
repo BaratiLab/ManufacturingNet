f55cfd0ef8035533397e66e308867faa0755507f
Lalit Ghule <58838043+lalitjg@users.noreply.github.com>
2020-11-11 15:23:48 -0500

    Update __init__.py

--- ManufacturingNet/models/__init__.py
+++ ManufacturingNet/models/__init__.py
@@ -5,12 +5,12 @@ from .xgb import XGBoost
 from .random_forest import RandomForest
 from .all_regression_models import AllRegressionModels
 from .all_classification_models import AllClassificationModels
-from .DNN import DNNModel
-from .CNN import CNN2D
+from .DNN import DNN
+from .CNN import CNN2DSignal
 from .CNN3D import CNN3D
-from .CNN_LSTM_Default import CNNLSTMModel
-from .CNN2DImage import CNN2DImageModel
-from .LSTM_Default import LSTMModel
+from .CNN_LSTM_Default import CNNLSTM
+from .CNN2DImage import CNN2DImage
+from .LSTM_Default import LSTM
 from .resnet import ResNet
 from .vgg import VGG
 from .densenet import DenseNet
@@ -20,4 +20,4 @@ from .mobilenet import MobileNet
 
 
 #__add__=['LinRegression','LogticRegression','MLP','SVM','XGBoost','RandomForest','AllRegressionModels','AllClassificationModels']
-__add__=['LinRegression','LogRegression','SVM','XGBoost','RandomForest','AllRegressionModels','AllClassificationModels','DNNModel','CNN2D', 'CNN3D', 'CNNLSTMModel', 'CNN2DImageModel', 'LSTMModel', 'ResNet','VGG','DenseNet','AlexNet','MobileNet','GoogleNet']
+__add__=['LinRegression','LogRegression','SVM','XGBoost','RandomForest','AllRegressionModels','AllClassificationModels','DNN','CNN2DSignal', 'CNN3D', 'CNNLSTM', 'CNN2DImage', 'LSTM', 'ResNet','VGG','DenseNet','AlexNet','MobileNet','GoogleNet']
