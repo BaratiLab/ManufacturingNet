cd59c8ae72c35ae7c315003aa7833576112389f9
Lalit Ghule <58838043+lalitjg@users.noreply.github.com>
2020-11-11 15:31:13 -0500

    Update LSTM_Default.py

--- ManufacturingNet/models/LSTM_Default.py
+++ ManufacturingNet/models/LSTM_Default.py
@@ -234,7 +234,7 @@ class LSTM(nn.Module):
 # The following class will be called by a user. The class calls other necessary classes to build a complete pipeline required for training
 class LSTM():
     """
-    Documentation Link:
+    Documentation Link:https://manufacturingnet.readthedocs.io/en/latest/
 
     """
 
