cb90471743a3e66fee54d170fba6df82dff72089
Ruchit <doshiruchit@gmail.com>
2020-11-24 20:58:15 -0500

    pip_package_0.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers=[
     'Intended Audience :: Education']
 
 setup(name=package_name,\
-    version='0.2.3',\
+    version='0.0.1',\
         description='AI and Machine Learning for manufacturing related datasets',
         long_description=LONG_DESCRIPTION,
         author='Ruchti Doshi',
