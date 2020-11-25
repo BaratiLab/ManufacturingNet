b6aa3096517f6d3cdec228dfbd5f924cc6b9ff5c
Lalit Ghule <58838043+lalitjg@users.noreply.github.com>
2020-11-24 20:32:52 -0500

    Update __init__.py

--- ManufacturingNet/datasets/__init__.py
+++ ManufacturingNet/datasets/__init__.py
@@ -2,6 +2,6 @@ from .datasets import PaderbornBearingData,CWRUBearingData
 from .datasets import CastingData,ChatterData  
 from .datasets import MercedesData, ThreeDPrintingData, MotorTempData, LithographyData
 from .datasets import GearboxData
-from .datasets import SpatterData
+#from .datasets import SpatterData
 
 __all__=['PaderbornBearingData','CWRUBearingData','CastingData','ChatterData','MercedesData', 'ThreeDPrintingData', 'MotorTempData', 'LithographyData','GearboxData','SpatterData']
